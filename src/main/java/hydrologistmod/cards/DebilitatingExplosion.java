61fc9d4d28e60f1942f332869d26412a61b3a8a2
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-10-01 00:04:47 -0400

    new card: Debilitating Explosion

---
+++ src/main/java/hydrologistmod/cards/DebilitatingExplosion.java
@@ -0,0 +1,58 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import com.megacrit.cardcrawl.powers.WeakPower;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+
+public class DebilitatingExplosion extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:DebilitatingExplosion";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/DebilitatingExplosion.png";
+    private static final int COST = 2;
+    private static final int AMOUNT_TO_APPLY = 2;
+    private static final int UPGRADE_AMOUNT = 1;
+
+    public DebilitatingExplosion() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.SKILL, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.COMMON, CardTarget.ALL_ENEMY);
+        tags.add(HydrologistTags.STEAM);
+        magicNumber = baseMagicNumber = AMOUNT_TO_APPLY;
+        exhaust = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (final AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
+            addToBot(new ApplyPowerAction(mo, p, new WeakPower(mo, magicNumber, false), magicNumber, true));
+            addToBot(new ApplyPowerAction(mo, p, new VulnerablePower(mo, this.magicNumber, false), magicNumber, true));
+        }
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new DebilitatingExplosion();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_AMOUNT);
+            exhaust = false;
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
