4c89a8dffc7c2f91d471a5873621d447a42059f4
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-09-30 23:13:29 -0400

    new card: Block of Ice

---
+++ src/main/java/hydrologistmod/cards/BlockOfIce.java
@@ -0,0 +1,48 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+
+public class BlockOfIce extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:BlockOfIce";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/BlockOfIce.png";
+    private static final int COST = 1;
+    private static final int BLOCK_AMT = 9;
+    private static final int UPGRADE_BLOCK_AMT = 3;
+
+    public BlockOfIce() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.SKILL, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.COMMON, CardTarget.SELF);
+        tags.add(HydrologistTags.ICE);
+        block = baseBlock = BLOCK_AMT;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new GainBlockAction(p, p, block));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new BlockOfIce();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBlock(UPGRADE_BLOCK_AMT);
+        }
+    }
+}
