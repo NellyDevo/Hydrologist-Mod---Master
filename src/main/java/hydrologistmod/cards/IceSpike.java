6d3962f5e98caecc8ea91b64f05ba903d1cbf2a3
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-09-30 20:17:50 -0400

    new Swappable pair: Tidal Wave || Ice Spike

---
+++ src/main/java/hydrologistmod/cards/IceSpike.java
@@ -0,0 +1,52 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import hydrologistmod.CardIgnore;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+
+@CardIgnore
+public class IceSpike extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:IceSpike";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/IceSpike.png";
+    private static final int COST = 1;
+    private static final int DAMAGE_AMT = 9;
+    private static final int UPGRADE_DAMAGE_AMT = 3;
+
+    public IceSpike() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.ATTACK, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.SPECIAL, CardTarget.ENEMY);
+        tags.add(HydrologistTags.ICE);
+        damage = baseDamage = DAMAGE_AMT;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new IceSpike();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_DAMAGE_AMT);
+        }
+    }
+}
---
+++ src/main/java/hydrologistmod/cards/TidalWave.java
@@ -0,0 +1,62 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import hydrologistmod.interfaces.SwappableCard;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+
+public class TidalWave extends AbstractHydrologistCard implements SwappableCard {
+    public static final String ID = "hydrologistmod:TidalWave";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/TidalWave.png";
+    private static final int COST = 1;
+    private static final int DAMAGE_AMT = 6;
+    private static final int UPGRADE_DAMAGE_AMT = 3;
+
+    public TidalWave() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.ATTACK, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.COMMON, CardTarget.ALL_ENEMY);
+        tags.add(HydrologistTags.WATER);
+        isMultiDamage = true;
+        baseDamage = DAMAGE_AMT;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAllEnemiesAction(p, multiDamage, DamageInfo.DamageType.NORMAL, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new TidalWave();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_DAMAGE_AMT);
+        }
+    }
+
+    @Override
+    public boolean hasDefaultPair() {
+        return true;
+    }
+
+    @Override
+    public AbstractCard createDefaultPair() {
+        return new IceSpike();
+    }
+}
