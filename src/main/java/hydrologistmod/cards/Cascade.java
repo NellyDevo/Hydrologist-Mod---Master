a2ba7e34f5cda836fc38e879d89fd8fb1e21e308
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-09-30 23:44:16 -0400

    new card: Cascade

---
+++ src/main/java/hydrologistmod/cards/Cascade.java
@@ -0,0 +1,53 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import hydrologistmod.actions.FlowAction;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+
+public class Cascade extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:Cascade";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/Cascade.png";
+    private static final int COST = 1;
+    private static final int BLOCK_AMT = 3;
+    private static final int UPGRADE_BLOCK_AMT = 1;
+
+    public Cascade() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.SKILL, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.COMMON, CardTarget.SELF);
+        tags.add(HydrologistTags.WATER);
+        block = baseBlock = BLOCK_AMT;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new FlowAction((int cardsDiscarded) -> {
+            for (int i = 0; i < cardsDiscarded; ++i) {
+                addToTop(new GainBlockAction(p, p, block));
+            }
+        }));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new Cascade();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBlock(UPGRADE_BLOCK_AMT);
+        }
+    }
+}
