12f48bc72946fc3ca7c26985e775baf1fd1ea782
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-10-02 00:26:03 -0400

    New Card: Hail

---
+++ src/main/java/hydrologistmod/actions/HailAction.java
@@ -0,0 +1,52 @@
+package hydrologistmod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
+import hydrologistmod.patches.HydrologistTags;
+
+public class HailAction extends AbstractGameAction {
+    private static final String ID = "hydrologistmod:HailAction";
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString(ID);
+    public static final String[] TEXT = uiStrings.TEXT;
+    private static final float DURATION = Settings.ACTION_DUR_FAST;
+    private AbstractCard callingCard;
+
+    public HailAction(AbstractCard card, int amount) {
+        duration = DURATION;
+        callingCard = card;
+        this.amount = amount;
+    }
+
+    @Override
+    public void update() {
+        if (duration == DURATION) {
+            AbstractPlayer p = AbstractDungeon.player;
+            callingCard.baseDamage += amount;
+            callingCard.applyPowers();
+            for (AbstractCard c : p.discardPile.group) {
+                if (c.hasTag(HydrologistTags.ICE)) {
+                    c.baseDamage += amount;
+                    c.applyPowers();
+                }
+            }
+            for (AbstractCard c : p.drawPile.group) {
+                if (c.hasTag(HydrologistTags.ICE)) {
+                    c.baseDamage += amount;
+                    c.applyPowers();
+                }
+            }
+            for (AbstractCard c : p.hand.group) {
+                if (c.hasTag(HydrologistTags.ICE)) {
+                    c.baseDamage += amount;
+                    c.applyPowers();
+                }
+            }
+        }
+        this.tickDuration();
+    }
+}
---
+++ src/main/java/hydrologistmod/cards/Hail.java
@@ -0,0 +1,56 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import hydrologistmod.actions.HailAction;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+
+public class Hail extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:Hail";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/Hail.png";
+    private static final int COST = 1;
+    private static final int DAMAGE_AMT = 3;
+    private static final int UPGRADE_DAMAGE = 1;
+    private static final int INCREASE_AMT = 3;
+    private static final int UPGRADE_INCREASE = 1;
+
+    public Hail() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.ATTACK, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.COMMON, CardTarget.ENEMY);
+        tags.add(HydrologistTags.ICE);
+        damage = baseDamage = DAMAGE_AMT;
+        magicNumber = baseMagicNumber = INCREASE_AMT;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+        addToBot(new HailAction(this, magicNumber));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new Hail();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_DAMAGE);
+            upgradeMagicNumber(UPGRADE_INCREASE);
+        }
+    }
+}
