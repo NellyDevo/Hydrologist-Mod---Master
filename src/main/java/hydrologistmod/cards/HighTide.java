54ef8a45571b7bdab7de946406a80ea553d3f39a
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-10-05 14:28:29 -0400

    new swappable pair: High Tide || Sea Foam

---
+++ src/main/java/hydrologistmod/cards/HighTide.java
@@ -0,0 +1,59 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import hydrologistmod.actions.FlowAction;
+import hydrologistmod.interfaces.SwappableCard;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+
+public class HighTide extends AbstractHydrologistCard implements SwappableCard {
+    public static final String ID = "hydrologistmod:HighTide";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/HighTide.png";
+    private static final int COST = 1;
+
+    public HighTide() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.ATTACK, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.UNCOMMON, CardTarget.ENEMY);
+        tags.add(HydrologistTags.WATER);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+        addToBot(new FlowAction());
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new HighTide();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+        }
+    }
+
+    @Override
+    public boolean hasDefaultPair() {
+        return true;
+    }
+
+    @Override
+    public AbstractCard createDefaultPair() {
+        return new SeaFoam();
+    }
+}
