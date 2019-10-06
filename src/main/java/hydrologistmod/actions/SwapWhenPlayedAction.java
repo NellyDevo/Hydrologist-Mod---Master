3113063a8e77987996199b222a4dbb39f83d1edb
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-10-06 14:21:44 -0400

    new swappable pair: Reservoir || Eruption

---
+++ src/main/java/hydrologistmod/actions/SwapWhenPlayedAction.java
@@ -0,0 +1,85 @@
+package hydrologistmod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.GameCursor;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.input.InputHelper;
+import com.megacrit.cardcrawl.localization.UIStrings;
+import helpers.SwapperHelper;
+import hydrologistmod.interfaces.SwappableCard;
+
+import java.lang.reflect.Field;
+
+public class SwapWhenPlayedAction extends AbstractGameAction {
+    private static final String ID = "hydrologistmod:SwapWhenPlayedAction";
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString(ID);
+    public static final String[] TEXT = uiStrings.TEXT;
+    private static final float DURATION = Settings.ACTION_DUR_FAST;
+    private AbstractCard card1;
+    private AbstractCard card2;
+
+    public SwapWhenPlayedAction(AbstractCard card) {
+        this.duration = DURATION;
+        card1 = card;
+        if (SwapperHelper.isCardRegistered(card)) {
+            card2 = SwapperHelper.getPairedCard(card);
+        } else {
+            isDone = true;
+            System.out.println("ERROR: why is SwapWhenPlayedAction being created on a non-paired card?");
+        }
+    }
+
+    @Override
+    public void update() {
+        if (!isDone) {
+            AbstractPlayer p = AbstractDungeon.player;
+            // find action, put card 2 into action, mark card 2 as card in use
+            UseCardAction useCardAction = null;
+            for (AbstractGameAction action : AbstractDungeon.actionManager.actions) {
+                if (action instanceof UseCardAction) {
+                    useCardAction = (UseCardAction)action;
+                    break;
+                }
+            }
+            if (useCardAction != null) {
+                try {
+                    Field targetCardField = UseCardAction.class.getDeclaredField("targetCard");
+                    targetCardField.setAccessible(true);
+                    if (targetCardField.get(useCardAction) == card1 && p.cardInUse == card1) {
+                        if (card1 instanceof SwappableCard) {
+                            ((SwappableCard) card1).onSwapOut();
+                        }
+                        targetCardField.set(useCardAction, card2);
+                        p.cardInUse = card2;
+                        card2.current_x = card1.current_x;
+                        card2.current_y = card1.current_y;
+                        card2.target_x = card1.target_x;
+                        card2.target_y = card1.target_y;
+                        if (card2 instanceof SwappableCard) {
+                            ((SwappableCard) card2).onSwapIn();
+                        }
+                        card2.isGlowing = card1.isGlowing;
+                        card1.isGlowing = false;
+                        card2.flash();
+                        for (AbstractCard handCard : p.hand.group) {
+                            handCard.applyPowers();
+                        }
+                    } else {
+                        System.out.println("ERROR: useCardAction targetCard and/or player.cardInUse is not the main card. Unable to swap.");
+                    }
+                } catch (NoSuchFieldException | IllegalAccessException e) {
+                    System.out.println("ERROR: target card reflection failed, unable to swap.");
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("ERROR: could not find a useCardAction! unable to Swap.");
+            }
+            this.isDone = true;
+        }
+    }
+}
---
+++ src/main/java/hydrologistmod/cards/Reservoir.java
@@ -0,0 +1,71 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.unique.LoseEnergyAction;
+import com.megacrit.cardcrawl.actions.utility.LoseBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import hydrologistmod.interfaces.SwappableCard;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+
+public class Reservoir extends AbstractHydrologistCard implements SwappableCard {
+    public static final String ID = "hydrologistmod:Reservoir";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/Reservoir.png";
+    private static final int COST = 1;
+    private static final int UPGRADED_COST = 0;
+    private static final int ENERGY_LOSS = 2;
+    private static final int UPGRADE_ENERGY_LOSS = -1;
+
+    public Reservoir() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.SKILL, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.RARE, CardTarget.NONE);
+        tags.add(HydrologistTags.WATER);
+        isInnate = true;
+        isEthereal = true;
+        magicNumber = baseMagicNumber = ENERGY_LOSS;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new Reservoir();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADED_COST);
+            upgradeMagicNumber(UPGRADE_ENERGY_LOSS);
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+
+    @Override
+    public boolean hasDefaultPair() {
+        return true;
+    }
+
+    @Override
+    public AbstractCard createDefaultPair() {
+        return new Eruption();
+    }
+
+    @Override
+    public void onSwapOut() {
+        addToBot(new LoseEnergyAction(magicNumber));
+    }
+}
