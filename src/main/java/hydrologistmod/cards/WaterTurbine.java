5650af8cef20b2bd81ada16eb3a6019ff9343ec4
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-10-08 18:38:20 -0400

    new card: Water Turbine

---
+++ src/main/java/hydrologistmod/cards/WaterTurbine.java
@@ -0,0 +1,48 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.EnergizedPower;
+import hydrologistmod.actions.FlowAction;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+
+public class WaterTurbine extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:WaterTurbine";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/WaterTurbine.png";
+    private static final int COST = 0;
+
+    public WaterTurbine() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.SKILL, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.RARE, CardTarget.SELF);
+        tags.add(HydrologistTags.WATER);
+        exhaust = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new FlowAction((int cardsDiscarded) -> addToTop(new ApplyPowerAction(p, p, new EnergizedPower(p, cardsDiscarded), cardsDiscarded))));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new WaterTurbine();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            exhaust = false;
+        }
+    }
+}
