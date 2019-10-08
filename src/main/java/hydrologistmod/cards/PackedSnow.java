a0d4cff72583c9709b24e8f8a1679849b5525a11
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-10-08 19:05:50 -0400

    new card: Packed Snow

---
+++ src/main/java/hydrologistmod/cards/PackedSnow.java
@@ -0,0 +1,52 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+import hydrologistmod.powers.PackedIcePower;
+import hydrologistmod.powers.PackedSteamPower;
+import hydrologistmod.powers.PackedWaterPower;
+
+public class PackedSnow extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:PackedSnow";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/PackedSnow.png";
+    private static final int COST = 3;
+    private static final int UPGRADED_COST = 2;
+
+    public PackedSnow() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.SKILL, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.RARE, CardTarget.SELF);
+        tags.add(HydrologistTags.ICE);
+        exhaust = true;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new PackedIcePower(p, 1), 1, true));
+        addToBot(new ApplyPowerAction(p, p, new PackedWaterPower(p, 1), 1, true));
+        addToBot(new ApplyPowerAction(p, p, new PackedSteamPower(p, 1), 1, true));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new PackedSnow();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBaseCost(UPGRADED_COST);
+        }
+    }
+}
---
+++ src/main/java/hydrologistmod/powers/PackedSteamPower.java
@@ -0,0 +1,75 @@
+package hydrologistmod.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardQueueItem;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import hydrologistmod.patches.HydrologistTags;
+
+public class PackedSteamPower extends AbstractPower implements CloneablePowerInterface {
+    public static final String POWER_ID = "hydrologistmod:PackedSteamPower";
+    public static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public PackedSteamPower(AbstractCreature owner, int amount) {
+        name = NAME;
+        ID = POWER_ID;
+        this.owner = owner;
+        region128 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("hydrologistmod/images/powers/PackedSteamPower84.png"), 0, 0, 84, 84);
+        region48 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("hydrologistmod/images/powers/PackedSteamPower32.png"), 0, 0, 32, 32);
+        type = PowerType.BUFF;
+        this.amount = amount;
+        updateDescription();
+    }
+
+    @Override
+    public void updateDescription() {
+
+    }
+
+    @Override
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        if (card.hasTag(HydrologistTags.STEAM) && !card.purgeOnUse && amount > 0) {
+            flash();
+            AbstractMonster m = null;
+            if (action.target != null) {
+                m = (AbstractMonster)action.target;
+            }
+            final AbstractCard tmp = card.makeSameInstanceOf();
+            AbstractDungeon.player.limbo.addToBottom(tmp);
+            tmp.current_x = card.current_x;
+            tmp.current_y = card.current_y;
+            tmp.target_x = Settings.WIDTH / 2.0f - 300.0f * Settings.scale;
+            tmp.target_y = Settings.HEIGHT / 2.0f;
+            if (tmp.cost > 0) {
+                tmp.freeToPlayOnce = true;
+            }
+            if (m != null) {
+                tmp.calculateCardDamage(m);
+            }
+            tmp.purgeOnUse = true;
+            AbstractDungeon.actionManager.cardQueue.add(new CardQueueItem(tmp, m, card.energyOnUse, true));
+            --amount;
+            if (amount == 0) {
+                AbstractDungeon.actionManager.addToBottom(new RemoveSpecificPowerAction(owner, owner, this));
+            }
+            updateDescription();
+        }
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new PackedSteamPower(owner, amount);
+    }
+}
