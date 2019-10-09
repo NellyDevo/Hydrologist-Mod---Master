6135a503d7fe8aa1a5231e959daecd341077c9e0
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-10-09 19:41:21 -0400

    new card: Flash Flood

--- src/main/java/hydrologistmod/cards/FlashFlood.java
+++ src/main/java/hydrologistmod/cards/FlashFlood.java
@@ -2,7 +2,6 @@ package hydrologistmod.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -14,36 +13,40 @@ import hydrologistmod.interfaces.TransmutableCard;
 import hydrologistmod.patches.AbstractCardEnum;
 import hydrologistmod.patches.HydrologistTags;
 
-public class IceBarrier extends AbstractHydrologistCard implements TransmutableCard {
-    public static final String ID = "hydrologistmod:IceBarrier";
+public class FlashFlood extends AbstractHydrologistCard implements TransmutableCard {
+    public static final String ID = "hydrologistmod:FlashFlood";
     public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
-    public static final String IMG_PATH = "hydrologistmod/images/cards/IceBarrier.png";
-    private static final int COST = 1;
-    public static final int BLOCK_AMT = 6;
-    private static final int UPGRADE_BLOCK_AMT = 3;
-    private static final int DAMAGE_AMT = 6;
-    private static final int UPGRADE_DAMAGE_AMT = 3;
+    public static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/FlashFlood.png";
+    private static final int COST = -2;
+    private static final int DAMAGE_AMT = 12;
+    private static final int UPGRADE_DAMAGE_AMT = 4;
 
-    public IceBarrier() {
+    public FlashFlood() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
-                AbstractCard.CardType.SKILL, AbstractCardEnum.HYDROLOGIST_CYAN,
-                AbstractCard.CardRarity.BASIC, AbstractCard.CardTarget.SELF);
-        block = baseBlock = BLOCK_AMT;
+                CardType.SKILL, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.UNCOMMON, CardTarget.NONE);
+        tags.add(HydrologistTags.WATER);
         magicNumber = baseMagicNumber = DAMAGE_AMT;
-        tags.add(HydrologistTags.ICE);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new GainBlockAction(p, p, block));
+
+    }
+
+    @Override
+    public boolean canUse(final AbstractPlayer p, final AbstractMonster m) {
+        this.cantUseMessage = EXTENDED_DESCRIPTION[0];
+        return false;
     }
 
     @Override
     public AbstractCard makeCopy() {
-        return new IceBarrier();
+        return new FlashFlood();
     }
 
     @Override
@@ -51,7 +54,6 @@ public class IceBarrier extends AbstractHydrologistCard implements TransmutableC
         if (!upgraded) {
             upgradeName();
             upgradeMagicNumber(UPGRADE_DAMAGE_AMT);
-            upgradeBlock(UPGRADE_BLOCK_AMT);
         }
     }
 
