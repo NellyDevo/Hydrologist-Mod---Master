6275164d6855974fc49097f6651fb46f219ab749
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-10-10 00:01:33 -0400

    new card: Condensation

---
+++ src/main/java/hydrologistmod/cards/Condensation.java
@@ -0,0 +1,61 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+import hydrologistmod.powers.CondensationPower;
+
+public class Condensation extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:Condensation";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/Condensation.png";
+    private static final int COST = 0;
+    private static final int DAMAGE_AMT = 3;
+    private static final int UPGRADE_DAMAGE = 2;
+    private static final int CARD_DRAW = 1;
+    private static final int UPGRADE_CARD_DRAW = 1;
+
+    public Condensation() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.ATTACK, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.UNCOMMON, CardTarget.ENEMY);
+        tags.add(HydrologistTags.ICE);
+        damage = baseDamage = DAMAGE_AMT;
+        magicNumber = baseMagicNumber = CARD_DRAW;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+        addToBot(new DrawCardAction(p, 1));
+        addToBot(new ApplyPowerAction(p, p, new CondensationPower(p, magicNumber)));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new Condensation();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_DAMAGE);
+            upgradeMagicNumber(UPGRADE_CARD_DRAW);
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
---
+++ src/main/java/hydrologistmod/powers/CondensationPower.java
@@ -0,0 +1,54 @@
+package hydrologistmod.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.NonStackablePower;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import hydrologistmod.patches.HydrologistTags;
+
+public class CondensationPower extends AbstractPower implements NonStackablePower, CloneablePowerInterface {
+    public static final String POWER_ID = "hydrologistmod:CondensationPower";
+    public static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public CondensationPower(AbstractCreature owner, int amount) {
+        name = NAME;
+        ID = POWER_ID;
+        this.owner = owner;
+        region128 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("hydrologistmod/images/powers/CondensationPower84.png"), 0, 0, 84, 84);
+        region48 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("hydrologistmod/images/powers/CondensationPower32.png"), 0, 0, 32, 32);
+        type = PowerType.BUFF;
+        this.amount = amount;
+        updateDescription();
+    }
+
+    @Override
+    public void updateDescription() {
+
+    }
+
+    @Override
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        AbstractDungeon.actionManager.addToTop(new RemoveSpecificPowerAction(owner, owner, this));
+        if (card.hasTag(HydrologistTags.WATER)) {
+            flash();
+            AbstractDungeon.actionManager.addToTop(new DrawCardAction(owner, amount));
+        }
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new CondensationPower(owner, amount);
+    }
+}
