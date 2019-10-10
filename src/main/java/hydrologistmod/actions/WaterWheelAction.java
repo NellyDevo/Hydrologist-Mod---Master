a578c286fa14c4f8c079d5e00dcf6269f8da3e18
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-10-10 00:42:25 -0400

    new card: Water Wheel

---
+++ src/main/java/hydrologistmod/actions/WaterWheelAction.java
@@ -0,0 +1,49 @@
+package hydrologistmod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
+import com.megacrit.cardcrawl.powers.EnergizedPower;
+import com.megacrit.cardcrawl.relics.ChemicalX;
+import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
+
+public class WaterWheelAction extends AbstractGameAction {
+    private static final String ID = "hydrologistmod:EncapsulatingIceAction";
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString(ID);
+    public static final String[] TEXT = uiStrings.TEXT;
+    private static final float DURATION = Settings.ACTION_DUR_FAST;
+    private int energyOnUse;
+    private boolean freeToPlayOnce;
+    private boolean upgraded;
+
+    public WaterWheelAction(int energyOnUse, boolean upgraded, boolean freeToPlayOnce) {
+        this.duration = DURATION;
+        this.energyOnUse = energyOnUse;
+        this.upgraded = upgraded;
+        this.freeToPlayOnce = freeToPlayOnce;
+    }
+
+    @Override
+    public void update() {
+        int effect = EnergyPanel.totalCount;
+        if (energyOnUse != -1) {
+            effect = energyOnUse;
+        }
+        if (upgraded) {
+            ++effect;
+        }
+        if (AbstractDungeon.player.hasRelic(ChemicalX.ID)) {
+            effect += 2;
+            AbstractDungeon.player.getRelic(ChemicalX.ID).flash();
+        }
+        if (effect > 0) {
+            AbstractDungeon.actionManager.addToTop(new ApplyPowerAction(AbstractDungeon.player, AbstractDungeon.player, new EnergizedPower(AbstractDungeon.player, effect), effect));
+            if (!freeToPlayOnce) {
+                AbstractDungeon.player.energy.use(EnergyPanel.totalCount);
+            }
+        }
+    }
+}
---
+++ src/main/java/hydrologistmod/cards/WaterWheel.java
@@ -0,0 +1,50 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import helpers.SwapperHelper;
+import hydrologistmod.actions.EncapsulatingIceAction;
+import hydrologistmod.actions.FlowAction;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+
+public class WaterWheel extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:WaterWheel";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/WaterWheel.png";
+    private static final int COST = -1;
+
+    public WaterWheel() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.SKILL, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.UNCOMMON, CardTarget.SELF);
+        tags.add(HydrologistTags.WATER);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new FlowAction());
+        addToBot(new EncapsulatingIceAction(SwapperHelper.getPairedCard(this), energyOnUse, upgraded, freeToPlayOnce));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new WaterWheel();
+    }
+
+    @Override
+    public void upgrade() {
+        SwapperHelper.upgrade(this);
+        if (!upgraded) {
+            upgradeName();
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
