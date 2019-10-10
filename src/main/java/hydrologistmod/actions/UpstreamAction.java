cce80b57db71b559e94894feb40593677a14744c
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-10-10 00:28:22 -0400

    new card: Upstream

---
+++ src/main/java/hydrologistmod/actions/UpstreamAction.java
@@ -0,0 +1,32 @@
+package hydrologistmod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.UIStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+public class UpstreamAction extends AbstractGameAction {
+    private static final String ID = "hydrologistmod:HydraulicsAction";
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString(ID);
+    public static final String[] TEXT = uiStrings.TEXT;
+    private static final float DURATION = Settings.ACTION_DUR_FAST;
+    private AbstractMonster monster;
+    private int blockAmt;
+
+    public UpstreamAction(AbstractMonster monster, int blockAmt) {
+        duration = DURATION;
+        this.monster = monster;
+        this.blockAmt = blockAmt;
+    }
+
+    @Override
+    public void update() {
+        if (monster != null && monster.getIntentBaseDmg() >= 0) {
+            AbstractDungeon.actionManager.addToTop(new GainBlockAction(AbstractDungeon.player, AbstractDungeon.player, blockAmt));
+        }
+        isDone = true;
+    }
+}
---
+++ src/main/java/hydrologistmod/cards/Upstream.java
@@ -0,0 +1,55 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.WeakPower;
+import hydrologistmod.actions.UpstreamAction;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+
+public class Upstream extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:Upstream";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/Upstream.png";
+    private static final int COST = 1;
+    private static final int WEAK_AMT = 2;
+    private static final int UPGRADE_WEAK = 1;
+    private static final int BLOCK_AMT = 5;
+    private static final int UPGRADE_BLOCK = 3;
+
+    public Upstream() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.SKILL, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.UNCOMMON, CardTarget.ENEMY);
+        tags.add(HydrologistTags.WATER);
+        magicNumber = baseMagicNumber = WEAK_AMT;
+        block = baseBlock = BLOCK_AMT;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(m, p, new WeakPower(m, magicNumber, false), magicNumber));
+        addToBot(new UpstreamAction(m, block));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new Upstream();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_WEAK);
+            upgradeBlock(UPGRADE_BLOCK);
+        }
+    }
+}
