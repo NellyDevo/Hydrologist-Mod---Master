ef0cdb906ed02de63e65c629a1e31d59aa50912c
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-10-10 18:36:41 -0400

    new card: Steam Engine

---
+++ src/main/java/hydrologistmod/cards/SteamEngine.java
@@ -0,0 +1,48 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+import hydrologistmod.powers.SteamEnginePower;
+
+public class SteamEngine extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:SteamEngine";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/SteamEngine.png";
+    private static final int COST = 2;
+
+    public SteamEngine() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.POWER, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.UNCOMMON, CardTarget.NONE);
+        tags.add(HydrologistTags.STEAM);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new SteamEnginePower(p, 1), 1));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new SteamEngine();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            isInnate = true;
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
---
+++ src/main/java/hydrologistmod/powers/SteamEnginePower.java
@@ -0,0 +1,84 @@
+package hydrologistmod.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.CardQueueItem;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import hydrologistmod.HydrologistMod;
+
+public class SteamEnginePower extends AbstractPower implements CloneablePowerInterface {
+    public static final String POWER_ID = "hydrologistmod:SteamEnginePower";
+    public static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+    private int count;
+
+    public SteamEnginePower(AbstractCreature owner, int amount) {
+        name = NAME;
+        ID = POWER_ID;
+        this.owner = owner;
+        region128 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("hydrologistmod/images/powers/SteamEnginePower84.png"), 0, 0, 84, 84);
+        region48 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("hydrologistmod/images/powers/SteamEnginePower32.png"), 0, 0, 32, 32);
+        type = PowerType.BUFF;
+        this.amount = amount;
+        updateDescription();
+        for (AbstractCard card : AbstractDungeon.actionManager.cardsPlayedThisTurn) {
+            if (!card.purgeOnUse && HydrologistMod.isThisCorporeal(card)) {
+                ++count;
+            }
+        }
+    }
+
+    @Override
+    public void updateDescription() {
+
+    }
+
+    @Override
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        if (!card.purgeOnUse && HydrologistMod.isThisCorporeal(card) && count < amount) {
+            flash();
+            AbstractMonster m = null;
+            if (action.target != null) {
+                m = (AbstractMonster)action.target;
+            }
+            AbstractCard tmp = card.makeSameInstanceOf();
+            AbstractDungeon.player.limbo.addToBottom(tmp);
+            tmp.current_x = card.current_x;
+            tmp.current_y = card.current_y;
+            tmp.target_x = Settings.WIDTH / 2.0f - 300.0f * Settings.scale;
+            tmp.target_y = Settings.HEIGHT / 2.0f;
+            if (tmp.cost > 0) {
+                tmp.freeToPlayOnce = true;
+            }
+            if (m == null && tmp.target == AbstractCard.CardTarget.ENEMY || tmp.target == AbstractCard.CardTarget.SELF_AND_ENEMY) {
+                m = AbstractDungeon.getRandomMonster();
+            }
+            if (m != null) {
+                tmp.calculateCardDamage(m);
+            }
+            tmp.purgeOnUse = true;
+            AbstractDungeon.actionManager.cardQueue.add(new CardQueueItem(tmp, m, card.energyOnUse, true));
+            ++count;
+        }
+    }
+
+    @Override
+    public void atStartOfTurn() {
+        count = 0;
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new SteamEnginePower(owner, amount);
+    }
+}
