261c4e5e806ce2d31c22c5c1eec9451d13b45f26
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-10-10 19:38:37 -0400

    New card: Hot Springs

---
+++ src/main/java/hydrologistmod/cards/HotSprings.java
@@ -0,0 +1,48 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+import hydrologistmod.powers.HotSpringsPower;
+
+public class HotSprings extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:HotSprings";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/HotSprings.png";
+    private static final int COST = 1;
+
+    public HotSprings() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.POWER, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.UNCOMMON, CardTarget.NONE);
+        tags.add(HydrologistTags.WATER);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new HotSpringsPower(p)));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new HotSprings();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            isInnate = true;
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
---
+++ src/main/java/hydrologistmod/powers/HotSpringsPower.java
@@ -0,0 +1,48 @@
+package hydrologistmod.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.BetterOnApplyPowerPower;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class HotSpringsPower extends AbstractPower implements CloneablePowerInterface {
+    public static final String POWER_ID = "hydrologistmod:HotSpringsPower";
+    public static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public HotSpringsPower(AbstractCreature owner) {
+        name = NAME;
+        ID = POWER_ID;
+        this.owner = owner;
+        region128 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("hydrologistmod/images/powers/HotSpringsPower84.png"), 0, 0, 84, 84);
+        region48 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("hydrologistmod/images/powers/HotSpringsPower32.png"), 0, 0, 32, 32);
+        type = PowerType.BUFF;
+        amount = -1;
+        updateDescription();
+    }
+
+    @Override
+    public void updateDescription() {
+
+    }
+
+    @Override
+    public void onApplyPower(AbstractPower p, AbstractCreature target, AbstractCreature source) {
+        if (target != source && source == owner && p.type == PowerType.DEBUFF && source.hasPower(p.ID)) {
+            flash();
+            AbstractDungeon.actionManager.addToTop(new RemoveSpecificPowerAction(source, source, p.ID));
+        }
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new HotSpringsPower(owner);
+    }
+}
