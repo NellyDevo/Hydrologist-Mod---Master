dcfff6deb4e89f8027e204e1315d00baa5da8a76
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-10-09 23:15:56 -0400

    new card: Glacial Synthesis

---
+++ src/main/java/hydrologistmod/cards/GlacialSynthesis.java
@@ -0,0 +1,50 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import hydrologistmod.actions.TransmuteCardAction;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+
+public class GlacialSynthesis extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:GlacialSynthesis";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/GlacialSynthesis.png";
+    private static final int COST = 1;
+    private static final int BLOCK_AMT = 6;
+    private static final int UPGRADE_BLOCK = 3;
+
+    public GlacialSynthesis() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.SKILL, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.UNCOMMON, CardTarget.SELF);
+        tags.add(HydrologistTags.ICE);
+        block = baseBlock = BLOCK_AMT;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new GainBlockAction(p, p, block));
+        addToBot(new TransmuteCardAction((AbstractCard potentialCard) -> potentialCard.hasTag(HydrologistTags.ICE)));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new GlacialSynthesis();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBlock(UPGRADE_BLOCK);
+        }
+    }
+}
