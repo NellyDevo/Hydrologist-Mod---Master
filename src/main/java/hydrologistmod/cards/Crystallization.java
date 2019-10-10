94a0b5001c92ef8adb209695764229e800e57e4e
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-10-10 00:01:59 -0400

    new card: Crystallization

---
+++ src/main/java/hydrologistmod/cards/Crystallization.java
@@ -0,0 +1,53 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+import hydrologistmod.powers.CrystallizationPower;
+
+public class Crystallization extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:Crystallization";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/Crystallization.png";
+    private static final int COST = 0;
+    private static final int BLOCK_AMT = 2;
+    private static final int UPGRADE_BLOCK = 2;
+
+    public Crystallization() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.SKILL, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.UNCOMMON, CardTarget.SELF);
+        tags.add(HydrologistTags.STEAM);
+        block = baseBlock = BLOCK_AMT;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new GainBlockAction(p, p, block));
+        addToBot(new DrawCardAction(p, 1));
+        addToBot(new ApplyPowerAction(p, p, new CrystallizationPower(p)));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new Crystallization();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeBlock(UPGRADE_BLOCK);
+        }
+    }
+}
---
+++ src/main/java/hydrologistmod/powers/CrystallizationPower.java
@@ -0,0 +1,54 @@
+package hydrologistmod.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.evacipated.cardcrawl.mod.stslib.powers.interfaces.NonStackablePower;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.actions.utility.UseCardAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import hydrologistmod.actions.TransmuteCardAction;
+import hydrologistmod.patches.HydrologistTags;
+
+public class CrystallizationPower extends AbstractPower implements NonStackablePower, CloneablePowerInterface {
+    public static final String POWER_ID = "hydrologistmod:CrystallizationPower";
+    public static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public CrystallizationPower(AbstractCreature owner) {
+        name = NAME;
+        ID = POWER_ID;
+        this.owner = owner;
+        region128 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("hydrologistmod/images/powers/CrystallizationPower84.png"), 0, 0, 84, 84);
+        region48 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("hydrologistmod/images/powers/CrystallizationPower32.png"), 0, 0, 32, 32);
+        type = PowerType.BUFF;
+        amount = -1;
+        updateDescription();
+    }
+
+    @Override
+    public void updateDescription() {
+
+    }
+
+    @Override
+    public void onUseCard(AbstractCard card, UseCardAction action) {
+        AbstractDungeon.actionManager.addToTop(new RemoveSpecificPowerAction(owner, owner, this));
+        if (card.hasTag(HydrologistTags.ICE)) {
+            flash();
+            AbstractDungeon.actionManager.addToTop(new TransmuteCardAction());
+        }
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new CrystallizationPower(owner);
+    }
+}
