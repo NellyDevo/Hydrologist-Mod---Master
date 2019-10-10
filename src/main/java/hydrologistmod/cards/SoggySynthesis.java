034ce051e889040e07e2d5165a12789641ec7c70
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-10-09 23:11:30 -0400

    new card: Soggy Synthesis

---
+++ src/main/java/hydrologistmod/cards/SoggySynthesis.java
@@ -0,0 +1,58 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import hydrologistmod.actions.TransmuteCardAction;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+
+public class SoggySynthesis extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:SoggySynthesis";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/SoggySynthesis.png";
+    private static final int COST = 1;
+    private static final int DAMAGE_AMT = 5;
+    private static final int BLOCK_AMT = 5;
+    private static final int UPGRADE_DAMAGE = 2;
+    private static final int UPGRADE_BLOCK = 2;
+
+    public SoggySynthesis() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.ATTACK, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.UNCOMMON, CardTarget.ENEMY);
+        tags.add(HydrologistTags.WATER);
+        damage = baseDamage = DAMAGE_AMT;
+        block = baseBlock = BLOCK_AMT;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new GainBlockAction(p, p, block));
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+        addToBot(new TransmuteCardAction((AbstractCard potentialCard) -> potentialCard.hasTag(HydrologistTags.WATER)));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new SoggySynthesis();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeDamage(UPGRADE_DAMAGE);
+            upgradeBlock(UPGRADE_BLOCK);
+        }
+    }
+}
