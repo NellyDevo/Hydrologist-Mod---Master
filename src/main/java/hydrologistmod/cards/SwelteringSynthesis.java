2427762f395573eb7eb3deaf7d529eac05ad9d54
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-10-09 23:04:08 -0400

    new card: Sweltering Synthesis

--- src/main/java/hydrologistmod/cards/SwelteringSynthesis.java
+++ src/main/java/hydrologistmod/cards/SwelteringSynthesis.java
@@ -8,40 +8,38 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import hydrologistmod.actions.FillHandWithCopiesAction;
 import hydrologistmod.actions.TransmuteCardAction;
 import hydrologistmod.patches.AbstractCardEnum;
 import hydrologistmod.patches.HydrologistTags;
 
-public class CrystalLattice extends AbstractHydrologistCard {
-    public static final String ID = "hydrologistmod:CrystalLattice";
+public class SwelteringSynthesis extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:SwelteringSynthesis";
     public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
-    public static final String IMG_PATH = "hydrologistmod/images/cards/CrystalLattice.png";
-    private static final int COST = 0;
-    private static final int DAMAGE_AMT = 5;
-    private static final int UPGRADE_DAMAGE = 3;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/SwelteringSynthesis.png";
+    private static final int COST = 1;
+    private static final int DAMAGE_AMT = 7;
+    private static final int UPGRADE_DAMAGE = 4;
 
-    public CrystalLattice() {
+    public SwelteringSynthesis() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
                 CardType.ATTACK, AbstractCardEnum.HYDROLOGIST_CYAN,
-                CardRarity.RARE, CardTarget.ENEMY);
-        tags.add(HydrologistTags.ICE);
+                CardRarity.UNCOMMON, CardTarget.ENEMY);
+        tags.add(HydrologistTags.STEAM);
         damage = baseDamage = DAMAGE_AMT;
-        exhaust = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
-        addToBot(new TransmuteCardAction((AbstractCard newCard) -> addToTop(new FillHandWithCopiesAction(newCard))));
+        addToBot(new TransmuteCardAction((AbstractCard potentialCard) -> potentialCard.hasTag(HydrologistTags.STEAM)));
     }
 
     @Override
     public AbstractCard makeCopy() {
-        return new CrystalLattice();
+        return new SwelteringSynthesis();
     }
 
     @Override
