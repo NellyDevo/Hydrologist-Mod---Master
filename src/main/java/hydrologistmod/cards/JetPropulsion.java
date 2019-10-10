417e7717644ba9f3c0b86318f76318115afdc8e5
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-10-10 00:36:13 -0400

    new card: Jet Propulsion

---
+++ src/main/java/hydrologistmod/cards/JetPropulsion.java
@@ -0,0 +1,51 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+import hydrologistmod.actions.JetPropulsionAction;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+
+public class JetPropulsion extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:JetPropulsion";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/JetPropulsion.png";
+    private static final int COST = 1;
+    private static final int MAGIC = 2;
+    private static final int UPGRADE_MAGIC = 1;
+
+    public JetPropulsion() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.SKILL, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.UNCOMMON, CardTarget.ENEMY);
+        tags.add(HydrologistTags.STEAM);
+        magicNumber = baseMagicNumber = MAGIC;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(m, p, new VulnerablePower(m, magicNumber, false), magicNumber));
+        addToBot(new JetPropulsionAction(m, magicNumber));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new JetPropulsion();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_MAGIC);
+        }
+    }
+}
