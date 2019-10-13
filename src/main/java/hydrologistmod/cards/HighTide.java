c3f1bff88ddda716918ad83da0e5867c11a88932
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2019-10-12 20:08:00 -0400

    Fix damage not initialized in High Tide

--- src/main/java/hydrologistmod/cards/HighTide.java
+++ src/main/java/hydrologistmod/cards/HighTide.java
@@ -22,12 +22,15 @@ public class HighTide extends AbstractHydrologistCard implements SwappableCard {
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
     public static final String IMG_PATH = "hydrologistmod/images/cards/HighTide.png";
     private static final int COST = 1;
+    private static final int DAMAGE_AMT = 7;
+    private static final int UPGRADE_DAMAGE = 3;
 
     public HighTide() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
                 CardType.ATTACK, AbstractCardEnum.HYDROLOGIST_CYAN,
                 CardRarity.UNCOMMON, CardTarget.ENEMY);
         tags.add(HydrologistTags.WATER);
+        damage = baseDamage = DAMAGE_AMT;
         SwapperHelper.registerPair(this, createDefaultPair());
     }
 
@@ -47,6 +50,7 @@ public class HighTide extends AbstractHydrologistCard implements SwappableCard {
         SwapperHelper.upgrade(this);
         if (!upgraded) {
             upgradeName();
+            upgradeDamage(UPGRADE_DAMAGE);
         }
     }
 
