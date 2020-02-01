fea23804c0ac50943797c4c312a5fb26d8bb4989
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2020-02-01 01:54:29 -0500

    make line follow the curves of the cards

--- src/main/java/hydrologistmod/vfx/TransmuteCardEffect.java
+++ src/main/java/hydrologistmod/vfx/TransmuteCardEffect.java
@@ -1,10 +1,7 @@
 package hydrologistmod.vfx;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
@@ -32,7 +29,9 @@ public class TransmuteCardEffect extends AbstractGameEffect {
     private HashMap<AbstractCard, TextureRegion> textureMap = new HashMap<>();
     private static TextureRegion mask = new TextureRegion(new Texture("hydrologistmod/images/vfx/TransmuteMask.png"), 512, 1024);
     private static TextureRegion line = new TextureRegion(new Texture("hydrologistmod/images/vfx/TransmuteLine.png"), 512, 1024);
-    private FrameBuffer fb;
+    private FrameBuffer fb1;
+    private FrameBuffer fb2;
+    private FrameBuffer fb3;
 
     public TransmuteCardEffect(HashMap<AbstractCard, AbstractCard> transmutedPairs, CardGroup.CardGroupType targetGroup, TransmuteCardAction action, float duration) {
         this.transmutedPairs = transmutedPairs;
@@ -40,22 +39,69 @@ public class TransmuteCardEffect extends AbstractGameEffect {
         this.targetGroup = targetGroup;
         this.duration = duration;
         this.startingDuration = duration;
-        fb = new FrameBuffer(Pixmap.Format.RGBA8888, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false, false);
+        fb1 = new FrameBuffer(Pixmap.Format.RGBA8888, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false, false);
+        fb2 = new FrameBuffer(Pixmap.Format.RGBA8888, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false, false);
+        fb3 = new FrameBuffer(Pixmap.Format.RGBA8888, Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), false, false);
     }
 
     @Override
     public void render(SpriteBatch sb) {
         //generate partial card images for each hash pair, using the mask, and overlay them over the cards positions along with transmute line effect
         //first, for each card, establish a new texture by creating a 512x512 framebuffer, and render the card, followed by the mask, at 256x256
-        for (AbstractCard card : transmutedPairs.values()) {
+        for (AbstractCard keyCard : transmutedPairs.keySet()) {
+            AbstractCard card = transmutedPairs.get(keyCard);
+            AbstractCard copyCard = keyCard.makeStatEquivalentCopy();
             sb.end();
 
-            fb.begin();
+            fb1.begin();
             Gdx.gl.glClearColor(0.0F, 0.0F, 0.0F, 0.0F);
             Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
             Gdx.gl.glColorMask(true,true,true,true);
             sb.begin();
-            //render the card at 256 x 256 on the frame buffer with a 1x scale, 0f rotation
+
+            //render both cards at 256 x 256 on the frame buffer, then capture the texture to be used as a mask
+            sb.setColor(Color.WHITE);
+            sb.setBlendFunction(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+            setCardAttributes(card);
+            card.render(sb);
+            setCardAttributes(copyCard);
+            copyCard.render(sb);
+
+            sb.end();
+            fb1.end();
+
+            TextureRegion tmpMask = new TextureRegion(fb1.getColorBufferTexture());
+            tmpMask.flip(false, true);
+
+            fb2.begin();
+            Gdx.gl.glClearColor(0.0F, 0.0F, 0.0F, 0.0F);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+            Gdx.gl.glColorMask(true,true,true,true);
+            sb.begin();
+
+            //render the line at offset to be masked
+            sb.setBlendFunction(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+            sb.setColor(Color.WHITE);
+            sb.draw(line, 0f, 0f - (512f * offsetPercent));
+
+            //render the tmp mask (not at offset) to mask the line, then capture the texture to be rendered over the final product
+            sb.setBlendFunction(0, GL20.GL_SRC_ALPHA);
+            sb.setColor(new Color(1,1,1,1));
+            sb.draw(tmpMask, 0f, 0f);
+
+            sb.end();
+            fb2.end();
+
+            TextureRegion tmpLine = new TextureRegion(fb2.getColorBufferTexture());
+            tmpLine.flip(false, true);
+
+            fb3.begin();
+            Gdx.gl.glClearColor(0.0F, 0.0F, 0.0F, 0.0F);
+            Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+            Gdx.gl.glColorMask(true,true,true,true);
+            sb.begin();
+
+            //render the card again, this time to be masked.
             sb.setColor(Color.WHITE);
             sb.setBlendFunction(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
             setCardAttributes(card);
@@ -66,16 +112,16 @@ public class TransmuteCardEffect extends AbstractGameEffect {
             sb.setColor(new Color(1,1,1,1));
             sb.draw(mask, 0f, 0f - (512f * offsetPercent));
 
-            //render the "water line" and "water effects" at offset
+            //render the "water line"
             sb.setBlendFunction(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
             sb.setColor(Color.WHITE);
-            sb.draw(line, 0f, 0f - (512f * offsetPercent));
-            //
+            sb.draw(tmpLine, 0f, 0f);
+            //render water effects at offset TODO
 
             sb.end();
-            fb.end();
+            fb3.end();
 
-            TextureRegion img = new TextureRegion(fb.getColorBufferTexture());
+            TextureRegion img = new TextureRegion(fb3.getColorBufferTexture());
             img.flip(false, true);
             textureMap.put(card, img);
             sb.begin();
@@ -101,7 +147,7 @@ public class TransmuteCardEffect extends AbstractGameEffect {
     @Override
     public void update() {
         if (duration == startingDuration) {
-            //Initialize: place the cards along the screen. Centered if only one card, but otherwise scattered similar to other game effects. Set hash pair render-relevant variables to be the same as the hash keys.
+            //Initialize: place the cards along the screen. Centered if only one card, but otherwise scattered similar to other game effects.
             AbstractDungeon.player.limbo.group.addAll(transmutedPairs.keySet());
             float cardCount = transmutedPairs.keySet().size();
             float alpha = Math.min(1.0f, (cardCount - 1) / ((float)MAX_CARD_COUNT - 1));
@@ -161,6 +207,8 @@ public class TransmuteCardEffect extends AbstractGameEffect {
 
     @Override
     public void dispose() {
-
+        fb1.dispose();
+        fb2.dispose();
+        fb3.dispose();
     }
 }
