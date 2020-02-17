f226fab2500ebd44a02f97f46f41369a51b7e95e
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2020-02-17 10:10:20 -0500

    Drying Pond: Renamed to Cryogenics, made into an Ice card, and its swappable pair, "Falling Icicles", was removed. Cost no longer upgrades to 0.

--- src/main/java/hydrologistmod/cards/Cryogenics.java
+++ src/main/java/hydrologistmod/cards/Cryogenics.java
@@ -1,59 +1,49 @@
 package hydrologistmod.cards;
 
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import hydrologistmod.helpers.SwapperHelper;
-import hydrologistmod.interfaces.SwappableCard;
+import hydrologistmod.actions.TransmuteCardAction;
 import hydrologistmod.patches.AbstractCardEnum;
 import hydrologistmod.patches.HydrologistTags;
 
-public class FallingIcicles extends AbstractHydrologistCard implements SwappableCard {
-    public static final String ID = "hydrologistmod:FallingIcicles";
+public class Cryogenics extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:Cryogenics";
     public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
-    public static final String IMG_PATH = "hydrologistmod/images/cards/FallingIcicles.png";
+    public static final String IMG_PATH = "hydrologistmod/images/cards/Cryogenics.png";
     private static final int COST = 1;
-    private static final int DAMAGE_AMT = 12;
-    private static final int UPGRADE_DAMAGE = 4;
 
-    public FallingIcicles() {
+    public Cryogenics() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
-                CardType.ATTACK, AbstractCardEnum.HYDROLOGIST_CYAN,
-                CardRarity.UNCOMMON, CardTarget.ENEMY);
+                CardType.SKILL, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.UNCOMMON, CardTarget.NONE);
         assignHydrologistSubtype(HydrologistTags.ICE);
-        damage = baseDamage = DAMAGE_AMT;
         exhaust = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.FIRE));
+        if (upgraded) {
+            addToBot(new DrawCardAction(p, 1));
+        }
+        addToBot(new TransmuteCardAction(true));
     }
 
     @Override
     public AbstractCard makeCopy() {
-        return new FallingIcicles();
+        return new Cryogenics();
     }
 
     @Override
     public void upgrade() {
-        SwapperHelper.upgrade(this);
         if (!upgraded) {
             upgradeName();
-            upgradeDamage(UPGRADE_DAMAGE);
         }
-    }
-
-    @Override
-    public boolean isPairCard() {
-        return true;
     }
 }
