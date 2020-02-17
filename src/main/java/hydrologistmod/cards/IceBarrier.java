1f061c07866f66ecb45f273a8872fe118ffe9b88
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2020-02-17 09:48:53 -0500

    Ice Barrier: instead of "when transmuted, deal damage", now simply applies cold to all enemies when played

--- src/main/java/hydrologistmod/cards/IceBarrier.java
+++ src/main/java/hydrologistmod/cards/IceBarrier.java
@@ -1,20 +1,18 @@
 package hydrologistmod.cards;
 
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import hydrologistmod.interfaces.TransmutableCard;
 import hydrologistmod.patches.AbstractCardEnum;
 import hydrologistmod.patches.HydrologistTags;
+import hydrologistmod.powers.ColdPower;
 
-public class IceBarrier extends AbstractHydrologistCard implements TransmutableCard {
+public class IceBarrier extends AbstractHydrologistCard {
     public static final String ID = "hydrologistmod:IceBarrier";
     public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
@@ -24,21 +22,25 @@ public class IceBarrier extends AbstractHydrologistCard implements TransmutableC
     private static final int COST = 1;
     public static final int BLOCK_AMT = 6;
     private static final int UPGRADE_BLOCK_AMT = 3;
-    private static final int DAMAGE_AMT = 6;
-    private static final int UPGRADE_DAMAGE_AMT = 3;
+    private static final int COLD_AMT = 3;
+    private static final int UPGRADE_COLD_AMT = 1;
 
     public IceBarrier() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
                 AbstractCard.CardType.SKILL, AbstractCardEnum.HYDROLOGIST_CYAN,
                 AbstractCard.CardRarity.BASIC, AbstractCard.CardTarget.SELF);
         block = baseBlock = BLOCK_AMT;
-        magicNumber = baseMagicNumber = DAMAGE_AMT;
+        magicNumber = baseMagicNumber = COLD_AMT;
         assignHydrologistSubtype(HydrologistTags.ICE);
+        tags.add(HydrologistTags.TEMPERATURE);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new GainBlockAction(p, p, block));
+        for (AbstractMonster mo : AbstractDungeon.getCurrRoom().monsters.monsters) {
+            addToBot(new ApplyPowerAction(mo, p, new ColdPower(mo, p, magicNumber), magicNumber));
+        }
     }
 
     @Override
@@ -50,17 +52,8 @@ public class IceBarrier extends AbstractHydrologistCard implements TransmutableC
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_DAMAGE_AMT);
+            upgradeMagicNumber(UPGRADE_COLD_AMT);
             upgradeBlock(UPGRADE_BLOCK_AMT);
         }
-    }
-
-    @Override
-    public void onTransmuted(AbstractCard newCard) {
-        int[] array = new int[AbstractDungeon.getMonsters().monsters.size()];
-        for (int i = 0; i < array.length; ++i) {
-            array[i] = magicNumber;
-        }
-        addToTop(new DamageAllEnemiesAction(AbstractDungeon.player, array, DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
     }
 }
