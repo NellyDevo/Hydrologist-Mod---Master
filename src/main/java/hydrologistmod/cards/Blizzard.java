decc77e544ec2c245cfed764fc0fd35cac79eb9e
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2020-04-16 14:50:31 -0400

    rework Hailstorm into Blizzard: instead of enemy taking damage, player gains block.

--- src/main/java/hydrologistmod/cards/Blizzard.java
+++ src/main/java/hydrologistmod/cards/Blizzard.java
@@ -9,45 +9,45 @@ import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import hydrologistmod.patches.AbstractCardEnum;
 import hydrologistmod.patches.HydrologistTags;
-import hydrologistmod.powers.HailstormPower;
+import hydrologistmod.powers.BlizzardPower;
 
-public class Hailstorm extends AbstractHydrologistCard {
-    public static final String ID = "hydrologistmod:Hailstorm";
+public class Blizzard extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:Blizzard";
     public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
-    public static final String IMG_PATH = "hydrologistmod/images/cards/Hailstorm.png";
+    public static final String IMG_PATH = "hydrologistmod/images/cards/Blizzard.png";
     private static final int COST = 2;
-    private static final int BLOCK_AMT = 10;
-    private static final int DAMAGE_AMT = 5;
-    private static final int UPGRADE_DAMAGE_AMT = 3;
+    private static final int BLOCK_AMT = 8;
+    private static final int POWER_AMT = 4;
+    private static final int UPGRADE_POWER_AMT = 2;
 
-    public Hailstorm() {
+    public Blizzard() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
                 CardType.SKILL, AbstractCardEnum.HYDROLOGIST_CYAN,
-                CardRarity.UNCOMMON, CardTarget.ENEMY);
+                CardRarity.UNCOMMON, CardTarget.SELF);
         assignHydrologistSubtype(HydrologistTags.ICE);
         block = baseBlock = BLOCK_AMT;
-        magicNumber = baseMagicNumber = DAMAGE_AMT;
+        magicNumber = baseMagicNumber = POWER_AMT;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new GainBlockAction(p, p, block));
-        addToBot(new ApplyPowerAction(m, p, new HailstormPower(m, magicNumber)));
+        addToBot(new ApplyPowerAction(p, p, new BlizzardPower(m, magicNumber)));
     }
 
     @Override
     public AbstractCard makeCopy() {
-        return new Hailstorm();
+        return new Blizzard();
     }
 
     @Override
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            upgradeMagicNumber(UPGRADE_DAMAGE_AMT);
+            upgradeMagicNumber(UPGRADE_POWER_AMT);
         }
     }
 }
--- src/main/java/hydrologistmod/powers/BlizzardPower.java
+++ src/main/java/hydrologistmod/powers/BlizzardPower.java
@@ -3,6 +3,7 @@ package hydrologistmod.powers;
 import basemod.interfaces.CloneablePowerInterface;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
@@ -14,19 +15,19 @@ import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import hydrologistmod.patches.HydrologistTags;
 
-public class HailstormPower extends AbstractPower implements CloneablePowerInterface {
+public class BlizzardPower extends AbstractPower implements CloneablePowerInterface {
     public static final String POWER_ID = "hydrologistmod:HailstormPower";
     public static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
-    public HailstormPower(AbstractCreature owner, int amount) {
+    public BlizzardPower(AbstractCreature owner, int amount) {
         name = NAME;
         ID = POWER_ID;
         this.owner = owner;
         region128 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("hydrologistmod/images/powers/HailstormPower84.png"), 0, 0, 84, 84);
         region48 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("hydrologistmod/images/powers/HailstormPower32.png"), 0, 0, 32, 32);
-        type = PowerType.DEBUFF;
+        type = PowerType.BUFF;
         this.amount = amount;
         updateDescription();
     }
@@ -40,12 +41,12 @@ public class HailstormPower extends AbstractPower implements CloneablePowerInter
     public void onUseCard(AbstractCard card, UseCardAction action) {
         if (card.hasTag(HydrologistTags.ICE)) {
             flash();
-            AbstractDungeon.actionManager.addToBottom(new DamageAction(owner, new DamageInfo(action.source, amount, DamageInfo.DamageType.THORNS)));
+            AbstractDungeon.actionManager.addToBottom(new GainBlockAction(owner, owner, amount));
         }
     }
 
     @Override
     public AbstractPower makeCopy() {
-        return new HailstormPower(owner, amount);
+        return new BlizzardPower(owner, amount);
     }
 }
