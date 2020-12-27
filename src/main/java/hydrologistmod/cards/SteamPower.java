ee84e2f281bfe9bca6666deaa8dad65d0ee36491
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2020-12-27 12:02:08 -0500

    fix crash with new SteamPower

--- src/main/java/hydrologistmod/cards/SteamPower.java
+++ src/main/java/hydrologistmod/cards/SteamPower.java
@@ -25,7 +25,7 @@ public class SteamPower extends AbstractHydrologistCard {
     public SteamPower() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
                 CardType.SKILL, AbstractCardEnum.HYDROLOGIST_CYAN,
-                CardRarity.COMMON, CardTarget.NONE);
+                CardRarity.COMMON, CardTarget.ENEMY);
         assignHydrologistSubtype(HydrologistTags.STEAM);
         exhaust = true;
     }
