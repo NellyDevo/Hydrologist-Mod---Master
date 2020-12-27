ab5aeca34f25ccb8ddd4352d8814475bd1b75bb1
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2020-12-26 20:49:15 -0500

    rework sweltering strike

--- src/main/java/hydrologistmod/cards/SwelteringStrike.java
+++ src/main/java/hydrologistmod/cards/SwelteringStrike.java
@@ -6,6 +6,7 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import hydrologistmod.HydrologistMod;
 import hydrologistmod.actions.HydrologistDamageAction;
 import hydrologistmod.patches.AbstractCardEnum;
 import hydrologistmod.patches.HydrologistTags;
@@ -18,8 +19,8 @@ public class SwelteringStrike extends AbstractHydrologistCard {
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
     public static final String IMG_PATH = "hydrologistmod/images/cards/SwelteringStrike.png";
     private static final int COST = 1;
-    private static final int DAMAGE_AMT = 10;
-    private static final int UPGRADE_DAMAGE_AMT = 4;
+    private static final int DAMAGE_AMT = 6;
+    private static final int UPGRADE_DAMAGE_AMT = 3;
 
     public SwelteringStrike() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
@@ -33,6 +34,12 @@ public class SwelteringStrike extends AbstractHydrologistCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new HydrologistDamageAction(getHydrologistSubtype(), m, new DamageInfo(p, damage, damageTypeForTurn)));
+        if (HydrologistMod.isCool(m)) {
+            addToBot(new HydrologistDamageAction(getHydrologistSubtype(), m, new DamageInfo(p, damage, damageTypeForTurn)));
+        }
+        if (HydrologistMod.isHot(m)) {
+            addToBot(new HydrologistDamageAction(getHydrologistSubtype(), m, new DamageInfo(p, damage, damageTypeForTurn)));
+        }
     }
 
     @Override
