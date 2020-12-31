c397f802bf333516bbe506e478c7c6eb4f1c9ccc
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2020-12-31 18:51:56 -0500

    remove the energy requirements from reservoir

--- src/main/java/hydrologistmod/cards/Reservoir.java
+++ src/main/java/hydrologistmod/cards/Reservoir.java
@@ -1,12 +1,10 @@
 package hydrologistmod.cards;
 
-import com.megacrit.cardcrawl.actions.unique.LoseEnergyAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.ui.panels.EnergyPanel;
 import hydrologistmod.helpers.SwapperHelper;
 import hydrologistmod.interfaces.SwappableCard;
 import hydrologistmod.patches.AbstractCardEnum;
@@ -23,11 +21,9 @@ public class Reservoir extends AbstractHydrologistCard implements SwappableCard 
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
     public static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
     public static final String IMG_PATH = "hydrologistmod/images/cards/Reservoir.png";
-    private static final int COST = 1;
+    private static final int COST = -2;
     private static final int UPGRADED_COST = 0;
-    private static final int ENERGY_LOSS = 2;
     private static final int UPGRADE_ENERGY_LOSS = -1;
-    private static String cantSwapMessage = EXTENDED_DESCRIPTION[0];
 
     public Reservoir() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
@@ -35,14 +31,18 @@ public class Reservoir extends AbstractHydrologistCard implements SwappableCard 
                 CardRarity.RARE, CardTarget.NONE);
         assignHydrologistSubtype(HydrologistTags.WATER);
         isInnate = true;
-        isEthereal = true;
-        magicNumber = baseMagicNumber = ENERGY_LOSS;
         SwapperHelper.makeSwappableGroup(new LinkedList<>(Arrays.asList(this, new Eruption())));
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
 
+    }
+
+    @Override
+    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
+        cantUseMessage = EXTENDED_DESCRIPTION[0];
+        return false;
     }
 
     @Override
@@ -60,20 +60,5 @@ public class Reservoir extends AbstractHydrologistCard implements SwappableCard 
             rawDescription = UPGRADE_DESCRIPTION;
             initializeDescription();
         }
-    }
-
-    @Override
-    public void onSwapOut() {
-        addToBot(new LoseEnergyAction(magicNumber));
-    }
-
-    @Override
-    public boolean canSwap() {
-        return EnergyPanel.totalCount >= magicNumber;
-    }
-
-    @Override
-    public String getUnableToSwapString() {
-        return cantSwapMessage;
     }
 }
