2397eb5c8af85afcecdd2bc7f1f29adcd0db7d06
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-01-04 01:25:53 -0500

    finish actually removing those powers

--- src/main/java/hydrologistmod/cards/AbstractHydrologistCard.java
+++ src/main/java/hydrologistmod/cards/AbstractHydrologistCard.java
@@ -11,14 +11,8 @@ import com.evacipated.cardcrawl.modthespire.lib.SpireOverride;
 import com.evacipated.cardcrawl.modthespire.lib.SpireSuper;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
-import com.megacrit.cardcrawl.powers.AbstractPower;
-import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import hydrologistmod.patches.HydrologistTags;
-import hydrologistmod.powers.SteamPowerIcePower;
-import hydrologistmod.powers.SteamPowerSteamPower;
-import hydrologistmod.powers.SteamPowerWaterPower;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -106,21 +100,6 @@ public abstract class AbstractHydrologistCard extends CustomCard {
             retVal.add(new TooltipInfo(BaseMod.getKeywordTitle(thermalShock.toLowerCase()), BaseMod.getKeywordDescription(thermalShock.toLowerCase())));
         }
         return retVal;
-    }
-
-    @Override
-    public boolean freeToPlay() {
-        if (AbstractDungeon.player != null && AbstractDungeon.currMapNode != null && AbstractDungeon
-                .getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT) {
-            for (AbstractPower power : AbstractDungeon.player.powers) {
-                if ((power instanceof SteamPowerWaterPower && hasTag(((SteamPowerWaterPower)power).tag))
-                || (power instanceof SteamPowerIcePower && hasTag(((SteamPowerIcePower)power).tag))
-                || (power instanceof SteamPowerSteamPower && hasTag(((SteamPowerSteamPower)power).tag))) {
-                    return true;
-                }
-            }
-        }
-        return super.freeToPlay();
     }
 
     @SpireOverride
