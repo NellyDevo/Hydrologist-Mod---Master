33ce9423d37b3bb8d5066ce6bed49f2b1169d280
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-01-19 23:06:16 -0500

    fix game crashing on load

--- src/main/java/hydrologistmod/cardmods/GainBlockEffect.java
+++ src/main/java/hydrologistmod/cardmods/GainBlockEffect.java
@@ -6,6 +6,7 @@ import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 
+@AbstractCardModifier.SaveIgnore
 public class GainBlockEffect extends AbstractExtraEffectModifier {
     public GainBlockEffect(AbstractCard card, boolean isMutable) {
         super(card, VariableType.BLOCK, isMutable);
