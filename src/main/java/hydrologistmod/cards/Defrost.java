a874ec4b0900b0b4d26f05637ce05352d2943d29
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-01-22 17:13:54 -0500

    new card: Defrost

---
+++ src/main/java/hydrologistmod/cards/Defrost.java
@@ -0,0 +1,48 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+import hydrologistmod.powers.DefrostPower;
+
+public class Defrost extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:Defrost";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/Defrost.png";
+    private static final int COST = 1;
+
+    public Defrost() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.POWER, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.UNCOMMON, CardTarget.NONE);
+        assignHydrologistSubtype(HydrologistTags.ICE);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new DefrostPower(p)));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new Defrost();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            isInnate = true;
+            rawDescription = UPGRADE_DESCRIPTION;
+            initializeDescription();
+        }
+    }
+}
---
+++ src/main/java/hydrologistmod/powers/DefrostPower.java
@@ -0,0 +1,62 @@
+package hydrologistmod.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.unique.RetainCardsAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import com.megacrit.cardcrawl.powers.EquilibriumPower;
+import com.megacrit.cardcrawl.relics.RunicPyramid;
+import hydrologistmod.HydrologistMod;
+
+public class DefrostPower extends AbstractPower implements CloneablePowerInterface {
+    public static final String POWER_ID = "hydrologistmod:DefrostPower";
+    public static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public DefrostPower(AbstractCreature owner) {
+        name = NAME;
+        ID = POWER_ID;
+        this.owner = owner;
+        region128 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("hydrologistmod/images/powers/DefrostPower84.png"), 0, 0, 84, 84);
+        region48 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("hydrologistmod/images/powers/DefrostPower32.png"), 0, 0, 32, 32);
+        type = PowerType.BUFF;
+        this.amount = -1;
+        updateDescription();
+    }
+
+    @Override
+    public void updateDescription() {
+        description = DESCRIPTIONS[0];
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new DefrostPower(owner);
+    }
+
+    @Override
+    public void atEndOfTurn(boolean isPlayer) {
+        if (isPlayer && !AbstractDungeon.player.hand.isEmpty() && !AbstractDungeon.player.hasRelic(RunicPyramid.ID)
+                && !AbstractDungeon.player.hasPower(EquilibriumPower.POWER_ID)) {
+            addToBot(new AbstractGameAction() {
+                @Override
+                public void update() {
+                    for (AbstractCard card : AbstractDungeon.player.hand.group) {
+                        if (HydrologistMod.isThisCorporeal(card) && !card.isEthereal) {
+                            card.retain = true;
+                        }
+                    }
+                    isDone = true;
+                }
+            });
+        }
+    }
+}
