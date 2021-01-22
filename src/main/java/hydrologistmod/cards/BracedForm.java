9839975a19d0c3e55e300a2344771089cb64f1f1
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-01-22 12:35:26 -0500

    new card: Braced Form

---
+++ src/main/java/hydrologistmod/cards/BracedForm.java
@@ -0,0 +1,51 @@
+package hydrologistmod.cards;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.unique.ApplyBulletTimeAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import hydrologistmod.patches.AbstractCardEnum;
+import hydrologistmod.patches.HydrologistTags;
+import hydrologistmod.powers.BracedFormPower;
+
+public class BracedForm extends AbstractHydrologistCard {
+    public static final String ID = "hydrologistmod:BracedForm";
+    public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String NAME = cardStrings.NAME;
+    public static final String DESCRIPTION = cardStrings.DESCRIPTION;
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String IMG_PATH = "hydrologistmod/images/cards/BracedForm.png";
+    private static final int COST = 3;
+    private static final int AMOUNT = 2;
+    private static final int UPGRADE_AMOUNT = 1;
+
+    public BracedForm() {
+        super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
+                CardType.POWER, AbstractCardEnum.HYDROLOGIST_CYAN,
+                CardRarity.RARE, CardTarget.NONE);
+        assignHydrologistSubtype(HydrologistTags.ICE);
+        magicNumber = baseMagicNumber = AMOUNT;
+        cardsToPreview = new RazorIce();
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new BracedFormPower(p, magicNumber), magicNumber));
+    }
+
+    @Override
+    public AbstractCard makeCopy() {
+        return new BracedForm();
+    }
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            upgradeMagicNumber(UPGRADE_AMOUNT);
+        }
+    }
+}
---
+++ src/main/java/hydrologistmod/powers/BracedFormPower.java
@@ -0,0 +1,49 @@
+package hydrologistmod.powers;
+
+import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.helpers.ImageMaster;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+import hydrologistmod.cards.RazorIce;
+
+public class BracedFormPower extends AbstractPower implements CloneablePowerInterface {
+    public static final String POWER_ID = "hydrologistmod:BracedFormPower";
+    public static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public BracedFormPower(AbstractCreature owner, int amount) {
+        name = NAME;
+        ID = POWER_ID;
+        this.owner = owner;
+        region128 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("hydrologistmod/images/powers/BracedFormPower84.png"), 0, 0, 84, 84);
+        region48 = new TextureAtlas.AtlasRegion(ImageMaster.loadImage("hydrologistmod/images/powers/BracedFormPower32.png"), 0, 0, 32, 32);
+        type = PowerType.BUFF;
+        this.amount = amount;
+        updateDescription();
+    }
+
+    @Override
+    public void atStartOfTurnPostDraw() {
+        addToBot(new MakeTempCardInHandAction(new RazorIce(), amount));
+    }
+
+    @Override
+    public void updateDescription() {
+        description = DESCRIPTIONS[0] + amount;
+        if (amount == 1) {
+            description += DESCRIPTIONS[1];
+        } else {
+            description += DESCRIPTIONS[2];
+        }
+    }
+
+    @Override
+    public AbstractPower makeCopy() {
+        return new BracedFormPower(owner, amount);
+    }
+}
