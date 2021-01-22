88594b26c2639b7b87b476f34b39f9326945e807
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-01-22 14:19:27 -0500

    why is this so much harder than it needs to be

--- src/main/java/hydrologistmod/cards/RazorIce.java
+++ src/main/java/hydrologistmod/cards/RazorIce.java
@@ -53,20 +53,22 @@ public class RazorIce extends AbstractHydrologistCard implements TransmutableCar
     //ebbing
     @Override
     public void triggerOnManualDiscard() {
-        AbstractMonster m = AbstractDungeon.getRandomMonster();
         AbstractDungeon.player.discardPile.removeCard(this);
         AbstractDungeon.player.limbo.addToTop(this);
-        target_y = Settings.HEIGHT / 2.0f + AbstractDungeon.miscRng.random(-100.0f, 300.0f);
-        target_x = Settings.WIDTH / 2.0f + AbstractDungeon.miscRng.random(-Settings.WIDTH / 4.0f, Settings.WIDTH / 4.0f);
-        targetAngle = 0;
+        addToBot(new AbstractGameAction() {
+            @Override
+            public void update() {
+                target_y = Settings.HEIGHT / 2.0f + AbstractDungeon.miscRng.random(-100.0f, 300.0f);
+                target_x = Settings.WIDTH / 2.0f + AbstractDungeon.miscRng.random(-Settings.WIDTH / 4.0f, Settings.WIDTH / 4.0f);
+                targetAngle = 0;
+                isDone = true;
+            }
+        });
         for (Soul soul : (ArrayList<Soul>)ReflectionHacks.getPrivate(AbstractDungeon.getCurrRoom().souls, SoulGroup.class, "souls")) {
             if (soul.card == this) {
                 soul.isDone = true;
                 break;
             }
-        }
-        if (m != null) {
-            calculateCardDamage(m);
         }
         AbstractDungeon.actionManager.addCardQueueItem(new CardQueueItem(this, true, energyOnUse, true, true), true);
     }
