e654f8fd1a88527c7c4334ac5c09d2d648985687
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-01-29 01:25:26 -0500

    rework pure water to use new Adaptive keyword

--- src/main/java/hydrologistmod/cards/PureWater.java
+++ src/main/java/hydrologistmod/cards/PureWater.java
@@ -6,43 +6,45 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import hydrologistmod.actions.FlowAction;
 import hydrologistmod.cardmods.AbstractExtraEffectModifier;
+import hydrologistmod.cardmods.AdaptiveEffect;
 import hydrologistmod.cardmods.DrawCardEffect;
-import hydrologistmod.cardmods.FlowEffect;
 import hydrologistmod.interfaces.TransmutableCard;
 import hydrologistmod.patches.AbstractCardEnum;
 import hydrologistmod.patches.HydrologistTags;
 
 import java.util.ArrayList;
 
-public class PureWater extends AbstractHydrologistCard implements TransmutableCard {
+public class PureWater extends AbstractAdaptiveCard implements TransmutableCard {
     public static final String ID = "hydrologistmod:PureWater";
     public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
     public static final String IMG_PATH = "hydrologistmod/images/cards/PureWater.png";
     private static final int COST = 0;
     private static final int DRAW_AMT = 1;
+    private static final int ADAPTIVE_AMT = 1;
+    private static final int UPGRADE_ADAPTIVE = 1;
 
     public PureWater() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
                 CardType.SKILL, AbstractCardEnum.HYDROLOGIST_CYAN,
-                CardRarity.UNCOMMON, CardTarget.SELF);
+                CardRarity.UNCOMMON, CardTarget.SELF, ADAPTIVE_AMT);
         assignHydrologistSubtype(HydrologistTags.WATER);
         magicNumber = baseMagicNumber = DRAW_AMT;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        if (upgraded) {
-            addToBot(new DrawCardAction(p, magicNumber));
-            addToBot(new FlowAction());
-        } else {
-            addToBot(new FlowAction());
-            addToBot(new DrawCardAction(p, magicNumber));
-        }
+        addToBot(new DrawCardAction(p, magicNumber));
+        super.use(p, m);
+    }
+
+    @Override
+    public String getDescription() {
+        return EXTENDED_DESCRIPTION[0];
     }
 
     @Override
@@ -54,21 +56,15 @@ public class PureWater extends AbstractHydrologistCard implements TransmutableCa
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            rawDescription = UPGRADE_DESCRIPTION;
-            initializeDescription();
+            upgradeAdaptiveNumber(UPGRADE_ADAPTIVE);
         }
     }
 
     @Override
     public ArrayList<AbstractExtraEffectModifier> getMutableAbilities() {
         ArrayList<AbstractExtraEffectModifier> list = new ArrayList<>();
-        if (upgraded) {
-            list.add(new DrawCardEffect(this, true, 1));
-            list.add(new FlowEffect(this, true, 1));
-        } else {
-            list.add(new FlowEffect(this, true, 1));
-            list.add(new DrawCardEffect(this, true, 1));
-        }
+        list.add(new DrawCardEffect(this, true, 1));
+        list.add(new AdaptiveEffect(this, true, 1));
         return list;
     }
 }
