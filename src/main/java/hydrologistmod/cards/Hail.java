616cc010e8a1fca67576d13b8e8f8eb39477aa70
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-01-29 01:18:00 -0500

    reworked Hail to use new Adaptive keyword

--- src/main/java/hydrologistmod/cards/Hail.java
+++ src/main/java/hydrologistmod/cards/Hail.java
@@ -6,38 +6,42 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import hydrologistmod.actions.HailAction;
 import hydrologistmod.actions.HydrologistDamageAction;
 import hydrologistmod.patches.AbstractCardEnum;
 import hydrologistmod.patches.HydrologistTags;
 
-public class Hail extends AbstractHydrologistCard {
+public class Hail extends AbstractAdaptiveCard {
     public static final String ID = "hydrologistmod:Hail";
     public static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
     public static final String NAME = cardStrings.NAME;
     public static final String DESCRIPTION = cardStrings.DESCRIPTION;
     public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    public static final String[] EXTENDED_DESCRIPTION = cardStrings.EXTENDED_DESCRIPTION;
     public static final String IMG_PATH = "hydrologistmod/images/cards/Hail.png";
     private static final int COST = 1;
-    private static final int DAMAGE_AMT = 3;
-    private static final int UPGRADE_DAMAGE = 1;
-    private static final int INCREASE_AMT = 3;
-    private static final int UPGRADE_INCREASE = 1;
+    private static final int DAMAGE_AMT = 7;
+    private static final int UPGRADE_DAMAGE = 3;
+    private static final int ADAPTIVE_AMT = 2;
+    private static final int UPGRADE_ADAPTIVE = 1;
 
     public Hail() {
         super(ID, NAME, IMG_PATH, COST, DESCRIPTION,
                 CardType.ATTACK, AbstractCardEnum.HYDROLOGIST_CYAN,
-                CardRarity.UNCOMMON, CardTarget.ENEMY);
+                CardRarity.UNCOMMON, CardTarget.ENEMY, ADAPTIVE_AMT);
         assignHydrologistSubtype(HydrologistTags.ICE);
         damage = baseDamage = DAMAGE_AMT;
-        magicNumber = baseMagicNumber = INCREASE_AMT;
         tags.add(HydrologistTags.CARES_ABOUT_SUBTYPES);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         addToBot(new HydrologistDamageAction(getHydrologistSubtype(), m, new DamageInfo(p, damage, damageTypeForTurn)));
-        addToBot(new HailAction(this, magicNumber));
+        super.use(p, m);
+    }
+
+    @Override
+    public String getDescription() {
+        return EXTENDED_DESCRIPTION[0];
     }
 
     @Override
@@ -50,7 +54,7 @@ public class Hail extends AbstractHydrologistCard {
         if (!upgraded) {
             upgradeName();
             upgradeDamage(UPGRADE_DAMAGE);
-            upgradeMagicNumber(UPGRADE_INCREASE);
+            upgradeAdaptiveNumber(UPGRADE_ADAPTIVE);
         }
     }
 }
