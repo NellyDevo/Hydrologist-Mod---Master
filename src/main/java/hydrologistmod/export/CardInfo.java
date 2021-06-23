86d253a6dcdbe8ec9f09455e34616c09888ccf68
JohnnyDevo <Jonathan.Devonshire@yahoo.com>
2021-06-23 09:34:44 -0400

    remove weird nulls

--- src/main/java/hydrologistmod/export/CardInfo.java
+++ src/main/java/hydrologistmod/export/CardInfo.java
@@ -1,12 +1,12 @@
 package hydrologistmod.export;
 
 public class CardInfo {
-    public String NAME;
-    public int COST;
-    public int UPGRADED_COST;
-    public String DESCRIPTION;
-    public String UPGRADED_DESCRIPTION;
-    public String TYPE;
-    public String SUBTYPE;
+    public String NAME = "";
+    public int COST = 0;
+    public int UPGRADED_COST = 0;
+    public String DESCRIPTION = "";
+    public String UPGRADED_DESCRIPTION = "";
+    public String TYPE = "";
+    public String SUBTYPE = "";
     public String[] KEYWORDS;
 }
